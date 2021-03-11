0a219a749b9a76f130b7360c611995c6e6ed6066
veronique khammisouk <verucaplay@hotmail.com>
2021-03-11 17:16:47 +0100

    formulaire + relation entity

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,10 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Form\ArticleFormType;
 use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BlogController extends AbstractController
@@ -18,7 +21,7 @@ class BlogController extends AbstractController
     public function home(): Response
     {
         return $this->render('blog/home.html.twig', [ // twig language de rendu
-            'title' => "Bienvenue sur le blog Symfony",
+            'title' => "Bienvenue sur le blog AnOther Symfony",
             'age' => 30
         ]);
     }
@@ -44,6 +47,7 @@ class BlogController extends AbstractController
         //outil de debugage de Symfony  (équivalent d'un var_dump en PHP)
         dump($repo);
 
+        // Doctrine selectionne en BDD tout les articles et les transmet au controller
         $articles = $repo->findAll();// equivalent de SELECT * FROM article + fetchAll
         dump($articles);
 
@@ -56,15 +60,107 @@ class BlogController extends AbstractController
 
     /**
      * Méthode permettant d'insérer et de modifier un article
+     * il est possible de définir plusiseur route qui execute la même méthode dans le controller
+     * 
      * @Route("/blog/new", name="blog_create")
+     * @Route("/blog/{id}/edit", name="blog_edit")
      */
-    public function create(Request $request): Response
+    public function create(Article $articleCreate = null, Request $request, EntityManagerInterface $manager): Response
     {
         // la classe Request de Symfony permet de véhiculer les données des superglobales PHP ($_POST, $_FILES, $_COOKIE, $_SESSION)
         // $request est un objet issu de la class Request injecté en dépendance de la méthode create()
         dump($request);
         
-        return $this->render('blog/create.html.twig');
+        //$request permet de stocker les données des superglobales, la propriété $request->request permet de stocker les données véhiculées par un forulaire ($_POST), ici on compte si il y a des données qui on été saisie dans le formulaire
+        // if($request->request->count() > 0)
+        // {
+            // Pour insérer dans la table Article, nous devons instancier un objet issu de ka classe entité Article, qui est lié à la table SQL Article
+            
+            // Si la variable $articleCreate N'EST PAS, si elle ne contient aucun article de la BDD, cela veut dire que nous avons envoyé la route '/blog/new', c'est une insertion, on entre dans le IF et on crée une nouvelle instance de l'entité Article, création d'un nouvel article
+            // Si la variable $articleCreate contient un article de la BDD, cela veut dire que nous avont envoyé la route '/blog/id/edit', c'est une modifiction d'article, on entre pas dans le IF
+            if(!$articleCreate)
+            {
+                $articleCreate = new Article;
+            }
+           
+
+            //On renseigne tout les setteurs de l'objet avec en argument les données du formulaire ($_POST)
+            // $articleCreate ->setTitle($request->request->get('title')) //$_POST['tilte']
+            //                 ->setContent($request->request->get('content'))
+            //                 ->setImage($request->request->get('image'))
+            //                 ->setCreatedAt(new \DateTime);
+        
+                            //dump($articleCreate);// On observe que l'objet entité Article $articleCreate, les propriétés contiennent bien les données du formaulaire
+
+                            // On fait appel au manager afin de pouvoir executer une insertion en BDD
+                            // $manager->persist($articleCreate);// on prépare et garde en mémoire l'insertion
+                            // $manager->flush();// on execute l'insertion
+
+                            //Après l'insertion, on redirige l'internaute vers le détail de l'article qui vient d'être inséré en BDD
+                            // Cela correspond à la route 'blog_show', mais c'est une route paramétrée qui attend un ID dans l'URL
+                            // En 2ème argument de redirectToRoute, nous transmettons l'ID de l'article qui vient d'être inséré en BDD
+                            // return $this->redirectToRoute('blog_show', [
+                            //     'id' => $articleCreate->getId() // dernier id inséré en BDD
+                            // ]);
+
+        //}
+        
+        
+        // creatFormBuilder() méthode de Symfony qui permet de générer un formulaire permettant de remplir une entité $articleCreate
+        // $form = $this->createFormBuilder($articleCreate)
+        //              ->add('title') // add() méthode qui permet de génrérer des champs de formulaire
+
+        //              ->add('content')
+
+        //              ->add('image')
+
+        //              ->getForm();// permet de valider le formulaire
+
+        //$articleCreate = new Article;
+        
+        // Ici nous renseignons le setter de l'objet et Symfony est capable automatiquement d'envoyer les valeurs de l'entité directement dans les attributs 'value' du formulaire, étant donné que l'entité $articleCreate est relié au formulaire
+        // $articleCreate->setTitle("Titre dump")
+        //               ->setContent("Contenu dump");
+        
+                      //Nous avons crée une class qui permet de générer le formulaire d'ajouter d'article, il faut dans le controller importer cette class ArticleFormType et relier le formulaire à notre entité Article $articleCreate
+        $form = $this->createForm(ArticleFormType::class, $articleCreate);
+ 
+        
+
+        // On pioche dans l'objet du formulaire la méthode handleRequest() qui permet de récupérer chaque données saisie dans le formulaire ($request) et de les binder, de les transmettre directement dans les bon setteurs de mon entité
+        
+        //  $_POST['title'] --> setTitle($_POST['title'])            
+        $form->handleRequest($request);
+
+        //dump($articleCreate);
+
+        //Si le formulaire a bien été soumit et que chauque valeur du formulaire ont bien  été transmises dans les bons setter de l'entité (isValid()), alors on entre dans le IF et on génère l'insertion
+        if($form->isSubmitted() && $form->isValid())
+        {
+            // On entre dans le IF seulement dans le cas où le formulaire a été validé et que chaque donnée st bien envoyé dans les bons setter de l'entité
+            // On appel le seter de la date, puisque nous n'avons pas de champs date dans le formulaire
+
+            // On entre dans le IF seulement dans le cas d'une insertion, et que l'article ne possède pas d'ID
+            if(!$articleCreate->getId())
+            {
+                $articleCreate->setCreatedAt(new \DateTime);
+            }
+            
+
+            $manager->persist($articleCreate); // on appel le manager pr préparer la requête d'insertion et la garder en mémoire
+            $manager->flush();// On exécute la requête d'insertion en BDD
+
+            return $this->redirectToRoute('blog_show', [
+                    'id' => $articleCreate->getId()
+            ]);
+
+        } 
+
+        return $this->render('blog/create.html.twig', [
+                'formArticle' => $form->createView(),// On transmet sur le template le formulaire, creatView() retourne un petit objet qui représente l'affichage du formulaire, on récupère sur le template create.html.twig
+
+                'editMode' => $articleCreate->getId() // cela permettra dans le template de savoir si l'article possède un ID ou non, si c'est une insertion ou une modification
+        ]);
 
     }
 
--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Article;
+use App\Entity\Category;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
@@ -10,30 +11,22 @@ class ArticleFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // Création de 10 'faux' articles
-        for($i=1; $i <= 10; $i++)
-        {
-            $article = new Article;
-            // Pour pouvoir insérer dans la table SQL 'Article', nous devons instancier un objet issu de cette class
-            // L'entité 'Article' reflète la table SQL 'Article'
-            // Nous avons besoin de renseigner tout les setteurs et tout les objets $article afin de pouvoir générer les insertion en BDD
+        // J'importe la librairie Faker installée via composer
+       $faker = \Faker\Factory::create('fr_FR');
 
-            //On remplit les objets articles grâce au setteurs
-            $article->setTitle("Titre de l'article n° $i")
-                    ->setContent("<p>Contenu de l'article $i </p>")
-                    ->setImage("https://picsum.photos/600/400")
-                    ->setCreatedAt(new \DateTime);// on instancie la class DateTime afin d'avoir automatiquement la date et l'heure dans la BDD
-            
-            // En Symfony, nous avons besoin d'un manager qui permet de manipuler les lignes de la BDD (insertion, modification, suppression)
-            // persist() est une méthode issue de la class ObjectManager qui permet de garder en mémoire les objets articles crées et préparer les requêtes d'insertion (INSERT INTO)       
-            $manager->persist($article);
-            
-        }
-        // flush() est une méthode issue de la classe ObjectManager qui permet véritablement d'executer les insertions en BDD (similaire à execute() en PHP)
-        $manager->flush();
+       //Création de 3 catégories
+
+       for($i = 1; $i <= 3; $i++)
+       {    
+            $category = new Category;
+
+            $category->setTitle($faker->sentence())
+                     ->setDescription($faker->paragraph());
+
+            $manager->persist($category);
 
 
-        // une fois les fixtures réaliseés, il faut les charger en BDD grace à doctrine (ORM) par la commande : 
-        // php bin/console doctrine:fixtures:load
+       }
+
     }
 }
